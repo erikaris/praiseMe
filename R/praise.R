9d336664d0ba371e355c6c1d9d04ef8b500f865f
erikaris <erika.mukhlisina@gmail.com>
2019-12-11 15:43:49 +1100

    2nd commit

--- R/praise.R
+++ R/praise.R
@@ -1,14 +1,16 @@
 #' praise
 #'
 #' @param name Name of person you want to praise
+#' @param punctuation the punctuation after the name
 #'
 #' @return text string
 #' @export
 #'
 #' @examples
 #'
-praise <- function(name = "asdf" ){
-  paste0("You're the best, ", name, "!")
+praise <- function(name = "default", punctuation = "!"){
+  # paste0("You're the best, ", name, punctuation)
+  glue::glue("you're the best, {name}{punctuation}")
 }
 
 

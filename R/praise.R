40e99a09ddf9493ddc04113c7ed554422e031b8b
erikaris <erika.mukhlisina@gmail.com>
2019-12-11 15:14:01 +1100

    first commit

---
+++ R/praise.R
@@ -0,0 +1,14 @@
+#' praise
+#'
+#' @param name Name of person you want to praise
+#'
+#' @return text string
+#' @export
+#'
+#' @examples
+#'
+praise <- function(name = "asdf" ){
+  paste0("You're the best, ", name, "!")
+}
+
+
